0d07f00c9f9a0932de71775757123200c7750a42
David Acosta <acosta816@gmail.com>
2020-11-13 14:46:30 -0800

    even better

---
+++ client/build/precache-manifest.4579d4f206bb06ef6ce9288acd39e98f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e95cdf490949f54fcfed15e47cb42e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b2d4b99c5372f93c8da",
+    "url": "/static/css/main.83648ea1.chunk.css"
+  },
+  {
+    "revision": "7fc2dc57fb72350394c0",
+    "url": "/static/js/2.46f3ea54.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.46f3ea54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2d4b99c5372f93c8da",
+    "url": "/static/js/main.7ae69177.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
